9e9413374fc668b7d545cb140329c895faeb8355
Hein Wai Htet <cuheinwaihtet@gmail.com>
2022-02-11 00:15:17 +0630

    adding composer in project

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,10 +1,10 @@
 <?php
 $config = require "config.php";
-require 'core/function.php';
-require 'core/Router.php';
-require 'core/Request.php';
-require 'core/databases/connection.php';
-require 'core/databases/QueryBuilder.php';
+// require 'core/function.php';
+// require 'core/Router.php';
+// require 'core/Request.php';
+// require 'core/databases/connection.php';
+// require 'core/databases/QueryBuilder.php';
 
 // create database connection
 $pdo = Connection::make($config["database"]);
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'b71c57924a68426fbe4cb7a098ff384f4f15bc35',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'b71c57924a68426fbe4cb7a098ff384f4f15bc35',
+            'dev_requirement' => false,
+        ),
+    ),
+);
